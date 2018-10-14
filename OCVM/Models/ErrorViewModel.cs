672366bfa7695e6e8a2dd2975604a480b2e6cef3
Emdadul Islam <emdadib3@gmail.com>
2018-10-14 22:53:13 +0600

    Add project files.

---
+++ OCVM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OCVM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
